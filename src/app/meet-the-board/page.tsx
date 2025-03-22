0516710c5e32916f4f12afaddec9a87a0fa6eed1
Devansh Tayal <tayalde1@msu.edu>
2025-03-22 18:38:45 -0400

    lazy loading for board on firstload

--- src/app/meet-the-board/page.tsx
+++ src/app/meet-the-board/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 export default function MeetTheEboard() {
     const eboardMembers = [
       { name: "Ankith Ram Mohan", role: "President", image: "/members/ankith.png", fav_word: "ಅಯ್ಯೋ" },
@@ -21,15 +22,21 @@ export default function MeetTheEboard() {
       <div className="max-w-7xl mx-auto px-4 py-20">
         <h1 className="text-4xl font-bold mb-8">Meet the Board</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          {eboardMembers.map((member) => (
-            <div key={member.name} className="card p-4">
-              <img src={member.image} alt={member.name} className="w-full h-auto rounded" />
-              <h3 className="mt-4 text-xl font-semibold">{member.name}</h3>
-              <p className="text-midnight-500">{member.role}</p>
-              {member.fav_word && <span className="text-base">Favorite Word: {member.fav_word}</span>}
-            </div>
-          ))}
-        </div>
+        {eboardMembers.map((member) => (
+          <div key={member.name} className="card p-4">
+            <Image src={member.image} alt={member.name} width={300} height={300}
+              className="w-full h-auto rounded"
+            />
+            <h3 className="mt-4 text-xl font-semibold">{member.name}</h3>
+            <p className="text-midnight-500">{member.role}</p>
+            {member.fav_word && (
+              <span className="text-base">
+                Favorite Word: {member.fav_word}
+              </span>
+            )}
+          </div>
+        ))}
+      </div>
   
         <h2 className="text-3xl font-bold mt-16 mb-8">Conference Committee</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
