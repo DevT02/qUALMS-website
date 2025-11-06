96e4e20498e3a3104a27f16be220ce52b64596a5
Devansh Tayal <devliveforever@gmail.com>
2025-11-05 21:42:43 -0500

    added favorite word

--- src/app/meet-the-board/page.tsx
+++ src/app/meet-the-board/page.tsx
@@ -6,9 +6,9 @@ export default function MeetTheEboard() {
       { name: "E. Sophia Normandin", role: "Vice President", image: "/members/sophia.png", fav_word: "tisquismiquis", alt: "Picture of Sophia" },
       { name: "Shashwat Krishna", role: "Treasurer", image: "/members/shashwat.png", fav_word: "je ne sais quoi", alt: "Picture of Shashwat" },
       { name: "Malosza Syrocki", role: "Secretary", image: "/members/malosza.png", fav_word: "bajo-bajo", alt: "Picture of Malosza" },
-      { name: "Guilherme Eckert Roda", role: "Event Coordinator", image: "/members/gui.jpeg", alt: "Picture of Guilherme"  },
+      { name: "Guilherme Eckert Roda", role: "Event Coordinator", fav_word:"Alô", image: "/members/gui.jpeg", alt: "Picture of Guilherme"  },
       { name: "Lyle", role: "Designer", image: "/members/lyle.png", fav_word: "boolean", alt: "Picture of Lyle" },
-      { name: "Devansh Tayal", role: "Tech/IT", image: "/members/devansh.jpg", fav_word: "nukilik", alt: "Picture of Devansh" },
+      { name: "Devansh Tayal", role: "Tech/IT", image: "/members/devansh.jpg", fav_word: "Nukilik", alt: "Picture of Devansh" },
       
       // Add additional members...
     ];  
